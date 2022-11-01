9be76aab7219b1f33ea21fb6805f08306dba30f1
edgar ludena <edgar.ludena90@gmail.com>
2022-11-01 17:03:23 -0400

    commit tree 524d73890bf6705b03f24c9d14b90b63645a2c58

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers){
+  return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers){
+  return drivers.slice(-2)
+} 
+
+const selectingDrivers =[ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiply){
+  return function (multi){
+    return multiply * multi
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+console.log(fareDoubler())
+
+const selectDifferentDrivers = function(drivers, returnDrivers){return returnDrivers(drivers)
+
+}
